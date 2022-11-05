8f436b4b68e6ee20f3f0e6f99555f0fe975db3d1
Abarna S <116026613+Abarna-6@users.noreply.github.com>
2022-11-05 21:32:18 +0530

    Create Subscriber.java
    The subscriber.java contains update() method, where once a new video or any input is notified by the observable, the method will gets updated in the observer method (Subscriber.java)

---
+++ Subscriber.java
@@ -0,0 +1,18 @@
+public class Subscriber 
+{
+    private String name;
+    private Channel channel=new Channel();
+    public Subscriber(String name)
+    {
+        super();
+        this.name=name;
+    }
+    public void update()
+    {
+        System.out.println("Hey, "+name+" video uploaded in "+channel.title);
+    }
+    public void subscribeChannel (Channel ch)
+    {
+        channel = ch;
+    }
+}
